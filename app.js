67e32efc9845e4c22858b9e201c0d34f7db394b8
Olivia Park <oliviaypark113@gmail.com>
2021-01-21 18:05:47 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const inquirer = require('inquirer');
+const fs = require('fs')
 
 const Employee = require('./lib/employee')
+const Manager = require('./lib/manager')
+// let department = process.argv[2];
+// let employName = process.argv[3];
 
 
 
@@ -16,6 +20,14 @@ const getPromptEmployee = ()=>{
     name:'name',
     validate:(value)=>{if(value){return true} else{return "Please enter valid information"}}
   },
+
+  {  type: "checkbox",
+  message:"What is employee's role?",
+  name: "role",
+  choices: ['Manager', 'Engineer', 'Intern'],
+  validate:(value)=>{if(value){return true} else{return "Please enter valid information"}},
+  
+  },
   
   {  type:'input',
   message:'What is your employee ID?',
@@ -30,21 +42,38 @@ const getPromptEmployee = ()=>{
   },
   
   
-  {  type: "checkbox",
-  message:"What is employee's role?",
-  name: "role",
-  choices: ['Fullstack Engineer', 'Front end developer', 'Back-end developer', 'Account'],
-  validate:(value)=>{if(value){return true} else{return "Please enter valid information"}},
-  
-  }
+ 
     ])
   }
   
   getPromptEmployee().then(info=>{
-      employeeArray.push(info)
-   console.log(employeeArray)
+  employeeArray.push(info)
+  console.log(employeeArray)
+  const employee = new Employee (info.name, info.id, info.email)
+  employee.getName("Olivia")
+  
+  console.log(employee)
+  //  fs.writeFile(`enployee.html`, html, err =>{if(err) throw err})
   
   
   })
+//   .catch(error => console.log(error))
 
 
+
+
+    // const rendertoHTML =employeeArray.map(employee=>{
+    //       return `
+          
+          
+          
+          
+          
+    //       `
+
+
+
+    // }).join("")
+
+
+   
